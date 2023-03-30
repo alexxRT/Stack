334d2408505d040be28166f7d107278cc9a02064
alexxRT <alekseev.aa@phystech.edu>
2023-03-30 10:13:31 +0300

    feature: crossplatform malloc/malloc.h

--- lib/Memory.h
+++ lib/Memory.h
@@ -3,17 +3,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc/malloc.h>
-
 
 
 static const void* POISNED_PTR = (const void*)0x78;
 static const char POISNED_ELEM = 0x77;
 
+#if defined(__APPLE__) //Mac OS X build
+#include <malloc/malloc.h>
+#define FREE( ptr ) { memset (ptr, POISNED_ELEM, malloc_size (ptr)); free (ptr); }
+
+#elif defined(__gnu_linux__) //Linux build
+#include <malloc.h>
+#define FREE( ptr ) { memset (ptr, POISNED_ELEM, malloc_usable_size(ptr)); free (ptr); }
+
+#elif defined(_WIN32) //Windows build, never tried, use mac os or linux instead ANYWAY
+#include <malloc.h>
+#define FREE( ptr ) { memset (ptr, POISNED_ELEM, _msize(ptr)); free (ptr); }
+
+#endif
+
+
 void* Recalloc (void* ptr, size_t new_size);
 
 #define CALLOC( count, type )            (type*)calloc(count, sizeof(type))
 #define RECALLOC( ptr, new_size, type )  (type*)Recalloc(ptr, new_size)
-#define FREE( ptr ) { memset (ptr, POISNED_ELEM, malloc_size (ptr)); free (ptr); }
 
 #endif
